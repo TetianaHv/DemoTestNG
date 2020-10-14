fd2e2086032303c48e2e41ac85dae2a014464f87
Tetiana Hv <tetiana.qaengineer@gmail.com>
2020-10-14 14:01:08 +0300

    New Rozetka Tests

---
+++ src/test/java/rozetka/RozetkaComparisonOfTwoMonitorsTest.java
@@ -0,0 +1,47 @@
+package rozetka;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import seleniumDemoLesson.BaseUITest;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.testng.Assert.assertEquals;
+
+public class RozetkaComparisonOfTwoMonitorsTest extends BaseUITest {
+    String url = "https://rozetka.com.ua/";
+    String searchText = "samsung";
+
+    @Test
+    public void basketIconTest() {
+        driver.get(url);
+        //list
+        driver.findElement(By.name("search")).sendKeys(searchText + Keys.ENTER);
+        By firstProductLink = By.cssSelector("a.goods-tile__picture");
+        wait.until(presenceOfElementLocated(firstProductLink));
+        driver.findElement(firstProductLink).click();
+        //product
+        By buyButtonSelector = By.cssSelector("button.buy-button.button.button_with_icon");
+        wait.until(presenceOfElementLocated(buyButtonSelector));
+        scrollToElement(driver.findElement(By.cssSelector("ul.product-statuses")));
+        assertEquals(driver.findElements(By.xpath("//a[@href='https:/rozetka.com.ua/cart/']/span")).size(), 0);
+        driver.findElement(buyButtonSelector).click();
+
+        //cart
+        driver.findElement(By.cssSelector("button.modal__close")).click();
+
+        WebElement basketIcon = driver.findElement(By.xpath("//a[@href='https:/rozetka.com.ua/cart/']/span"));
+        assertEquals(basketIcon.getText(), 1);
+    }
+
+    private void scrollToElement(WebElement element) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/rozetka/RozetkaFiltersTests.java
@@ -0,0 +1,4 @@
+package rozetka;
+
+public class RozetkaFiltersTests {
+}
