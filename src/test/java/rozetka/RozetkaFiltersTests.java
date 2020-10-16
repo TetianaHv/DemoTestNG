06208036aeb6813c2b2d54261103f236b67a7802
Tetiana Hv <tetiana.qaengineer@gmail.com>
2020-10-16 16:59:50 +0300

    productFilterTest done

--- src/test/java/rozetka/RozetkaFiltersTests.java
+++ src/test/java/rozetka/RozetkaFiltersTests.java
@@ -4,12 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.testng.annotations.Test;
 import seleniumDemoLesson.BaseUITest;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import java.util.List;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class RozetkaFiltersTests extends BaseUITest {
     String url = "https://rozetka.com.ua/";
@@ -23,17 +27,35 @@ public class RozetkaFiltersTests extends BaseUITest {
 5. Verify all filtered products are products made by Samsung, Apple or Honor
  */
     @Test
-    public void productFilterTest() {
+    public void productFilterTest() throws Exception {
         driver.get(url);
         driver.findElement(By.xpath("//input[@name='search']")).sendKeys(searchText + Keys.ENTER);
         By block = By.xpath("//span[@class='categories-filter__link-title' and contains(text(), 'Мобильные телефоны')]");
         wait.until(presenceOfElementLocated(block));
         driver.findElement(block).click();
-        wait.until(presenceOfElementLocated(By.className("sidebar-block")));
-        WebElement checkboxApple = driver.findElement(By.xpath("//input[@id='Apple']"));
-        scrollToElement(checkboxApple);
-        checkboxApple.click();
 
+        wait.until(presenceOfElementLocated(By.className("sidebar-block")));
+        scrollToElement(driver.findElement(By.cssSelector("div.sidebar-alphabet")));
+        WebElement checkboxApple = driver.findElement(By.id("Apple"));
+        Actions act = new Actions(driver);
+        act.moveToElement(checkboxApple).click().build().perform();
+
+        wait.until(presenceOfElementLocated(By.className("sidebar-block")));
+        scrollToElement(driver.findElement(By.cssSelector("div.sidebar-alphabet")));
+        WebElement checkboxHonor = driver.findElement(By.id("Honor"));
+        act.moveToElement(checkboxHonor).click().build().perform();
+
+        By list = By.className("goods-tile__heading");
+        wait.until(presenceOfAllElementsLocatedBy(list));
+        List<WebElement> listOfNames = driver.findElements(list);
+        for (int i = 0; i < listOfNames.size(); i++) {
+            String itemName = listOfNames.get(i).getAttribute("title");
+            if (itemName.contains("Samsung") || itemName.contains("Apple") || itemName.contains("Honor")) {
+                continue;
+            } else {
+                throw new Exception(itemName + " does not contain Samsung, Apple or Honor");
+            }
+        }
     }
 
     /*
